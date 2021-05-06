781d3550f28bf527ae6108ce7f94ae2f2a2299a5
Your Name <megatron@example.com>
2021-05-06 12:26:13 -0300

    First commit

---
+++ src/test/java/com/davidreiso/dev/WebProject/WebProjectApplicationTests.java
@@ -0,0 +1,13 @@
+package com.davidreiso.dev.WebProject;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WebProjectApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
